f9595c93af3a85172490f84f139ca7ed28caa394
tzulingtseng <y3y6xu4@cengzilingde-MacBook-Air.local>
2021-08-01 13:31:23 +0800

    better 1~10 function

--- basic/sum.js
+++ basic/sum.js
@@ -1,10 +1,21 @@
-console.log("Hello world!");
+//console.log("Hello world!");
 
-function sum(n){
-    let result = 0;
-    for(let i=1; i<=n; i++){
-        result+=i;
-    }
-    return result;
+function sum(n) {
+  //最佳美容解法．不用result變數
+  return ((n + 1) * n) / 2;
+
+  //最佳解法
+  // let result = 0;
+  // result = ((n+1)*n)/2;
+  // return result;
+
+  //迴圈法
+  //let result = 0;
+  // for(let i=1; i<=n; i++){
+  //     result+=i;
+  // }
+  //return result;
 }
-console.log(sum(10))
+// console.log(sum(1)); //1
+// console.log(sum(2)); //3
+// console.log(sum(10)); //55
