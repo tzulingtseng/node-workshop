456acb77f204b3617ee59423601d1238c252d39e
tzulingtseng <y3y6xu4@cengzilingde-MacBook-Air.local>
2021-08-07 16:25:07 +0800

    delete question mark on URL

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -3,7 +3,7 @@ const moment = require("moment"); //引用moment套件
 // console.log(moment().format("YYYYMMDD"));
 
 axios
-  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?", {
+  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
     params: {
       response: "json",
       date: moment().format("YYYYMMDD"), //動態日期
