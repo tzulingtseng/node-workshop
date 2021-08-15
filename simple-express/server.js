3592e2ad7cec20ff81cf7d9e9b7009d63a624670
tzulingtseng <y3y6xu4@cengzilingde-MacBook-Air.local>
2021-08-15 11:41:49 +0800

    開發兩個 middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,12 +3,23 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
+let current = new Date();
+app.use((req, res, next) => {
+  console.log(`有人來訪問 at ${current.toISOString()}`);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`第二個中間件`);
+  next();
+});
+
 // HTTP Method: get, post, put, patch, delete
 app.get("/", function (request, response, next) {
   response.send("Hello");
 });
 app.get("/about", function (request, response, next) {
-  response.send("about");
+  response.send("about111");
 });
 
 app.listen(3000, function () {
